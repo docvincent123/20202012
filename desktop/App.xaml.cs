b00f13a77f57fbace97700434c5cc3b6a986ddf6
docvincent123 <mishaborkovskij9@gmail.com>
2026-09-05 18:08:33 +0300

    Add WPF startup

---
+++ desktop/App.xaml.cs
@@ -0,0 +1,3 @@
+using System.Windows;
+namespace RehaFlow.CommandCenter;
+public partial class App : Application { protected override void OnStartup(StartupEventArgs e){ base.OnStartup(e); MainWindow=new MainWindow(); MainWindow.Show(); } }
