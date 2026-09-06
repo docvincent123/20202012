b89cb646d48be1ec6a96167046783dbd43bef39f
docvincent123 <mishaborkovskij9@gmail.com>
2026-09-06 11:42:07 +0300

    Route BaaS through migration-safe canonical API

--- netlify/functions/baas.ts
+++ netlify/functions/baas.ts
@@ -1,5 +1,5 @@
 import type { Config, Handler } from '@netlify/functions';
-import { handler as apiHandler } from './api3';
+import { handler as apiHandler } from './api4';
 
 export const config: Config = { path: ['/api/baas', '/api/baas/*'] };
 
